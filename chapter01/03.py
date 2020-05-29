0883e190fa9a5ae1441e1a64eff099d28cfea9a5
yuusaku <yuusaku@example.com>
2020-05-29 20:11:13 +0900

    課題

---
+++ chapter01/03.py
@@ -0,0 +1,8 @@
+str1 = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+str2 = str1.split()
+str3 = []
+for i in range(len(str2)):
+    str3 += str(len(str2[i]))
+print(str3)
+
+
