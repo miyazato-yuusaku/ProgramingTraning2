72227beffb3d70a058272c79ae044e53d2a6f503
yuusaku <yuusaku@example.com>
2020-06-08 21:56:27 +0900

    teisei

--- chapter01/03.py
+++ chapter01/03.py
@@ -1,5 +1,5 @@
 str1 = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-str2 = str1.split()
+str2 = str1.replace(',', '').replace('.', '').split()
 str3 = []
 for i in range(len(str2)):
     str3 += str(len(str2[i]))
